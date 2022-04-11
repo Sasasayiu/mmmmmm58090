7d58149e731c1ee9797bc8eafdc6c28384152670
Sasasayiu <103177369+Sasasayiu@users.noreply.github.com>
2022-04-12 00:34:25 +0600

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from requests import post 
-from fake_headers import Headers
-import requests
-
-data = post('https://discord.com/api/v9/auth/register/phone',json={"phone":"+79165885068"},proxies={'http://':'66.23.232.83:3128'})
-
-print(data.text)
